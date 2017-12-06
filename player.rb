41366f23653ede8e143888d868b0acf75df1f234
V. Anastassiou <v.anastassiou@gmail.com>
2017-12-06 04:12:43 +0000

    Removed superfluous comments

--- player.rb
+++ player.rb
@@ -1,16 +1,13 @@
 class Player
 
-# Each player must have an identity and a score value
   def initialize(name)
     @name = name
-    @lives = 20
+    @lives = 3
   end
 
-  # Game needs to get/set name, get score
   attr_accessor :name
   attr_reader :lives
 
-  # Method to increment score
   def lose_life
     @lives -= 1
   end
