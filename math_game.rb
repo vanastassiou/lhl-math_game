d0e089095decb812eaa07731f9e8ef101e1949db
V. Anastassiou <v.anastassiou@gmail.com>
2017-12-06 01:58:50 +0000

    Added skeletons of necessary methods

--- math_game.rb
+++ math_game.rb
@@ -10,4 +10,26 @@ class MathGame
     @whose_turn = 0
   end
 
+  def current_player
+    @players[@whose_turn]
+  end
+
+  def lose_life
+    current_player.lose_life
+  end
+
+  def game_over
+
+  end
+
+
+  def start_game
+
+  end
+
+
+  def next_turn
+
+  end
+
 end
