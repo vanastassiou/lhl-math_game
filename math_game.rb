f3601b0f3636fdf7c163ee2a3206638e069b278d
V. Anastassiou <v.anastassiou@gmail.com>
2017-12-06 02:10:01 +0000

    Set gameover condition

--- math_game.rb
+++ math_game.rb
@@ -18,18 +18,28 @@ class MathGame
     current_player.lose_life
   end
 
-  def game_over
-
+  def game_over?
+    @players.any? do |player|
+      player.lives == 0
+    end
   end
 
 
   def start_game
-
+    until game_over? do
+      puts "Are you ready #{current_player.name}?"
+      gets
+      # TODO ask a question and only award point if correct
+      award_point
+      swap_players
+      print_scores
+    end
   end
 
 
   def next_turn
 
   end
+
 
 end
